6f529c66ed17e482211fed78027d6a1dd3e1a757
K <EMAIL>
2024-12-03 22:52:26 -0700

    finished day 3

--- 2024/day_03/part1.py
+++ 2024/day_03/part1.py
@@ -7,7 +7,6 @@ raw_data = open("input.txt", 'r', encoding='utf-8').read()
 pattern = r"mul\((\d+),(\d+)\)"
 
 matches = re.findall(pattern, raw_data)
-# print(matches)
 
 products = [int(a) * int(b) for a, b in matches]
 
